13c80d1efe20f6098b730addd93cd7f4cf0a6290
Murilo Freire <murilofreire777@gmail.com>
2025-03-25 21:17:19 -0300

    (feat) new code

--- model/User.py
+++ model/User.py
@@ -1,5 +1,6 @@
 class User:
-    def __init__(self, nome, cpf, endereco):
+    def __init__(self, nome, cpf, rg, endereco):
         self.nome = nome
         self.cpf = cpf
+        self.rg = rg
         self.endereco = endereco
